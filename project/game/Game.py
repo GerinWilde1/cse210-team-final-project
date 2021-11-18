afde590bbe3d56f3bb90d11f5c75b9686ea5af32
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-18 16:41:04 -0600

    Update Game.py

--- project/game/Game.py
+++ project/game/Game.py
@@ -12,24 +12,24 @@ import random
 
 class Game(arcade.Window):
 
-def __init__(self, width, height):
+    def __init__(self, width, height):
 
-def on_draw(self):
+    def on_draw(self):
 
-def update(self, delta_time):
+    def update(self, delta_time):
 
-def create_ships(self):
+    def create_ships(self):
 
-def check_collisions(self):
+    def check_collisions(self):
 
-def cleanup_zombies(self):
+    def cleanup_zombies(self):
 
-def break_apart(self):
+    def break_apart(self):
 
-def draw_gameover(self):
+    def draw_gameover(self):
 
-def check_keys(self):
+    def check_keys(self):
 
-def on_key_press(self, key: int, modifiers: int):
+    def on_key_press(self, key: int, modifiers: int):
 
-def on_key_release(self, key: int, modifiers: int):
+    def on_key_release(self, key: int, modifiers: int):

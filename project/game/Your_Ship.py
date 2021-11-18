d23cb3b9449e78c724a9b9d722dea6f373d3f975
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-18 12:15:09 -0600

    Update Your_Ship.py

--- project/game/Your_Ship.py
+++ project/game/Your_Ship.py
@@ -3,7 +3,6 @@ import constants
 import Moving_Object
 import Position
 from project.game.Moving_Object import Flying_Object
-from project.game.constants import BIG_BOAT_SPEED
 import math
 
 
@@ -11,6 +10,8 @@ class Ship(Flying_Object):
 
     def __init__(self):
         super().__init__()
+        self.center.x = constants.SCREEN_WIDTH // 2
+        self.center.y = constants.SCREEN_HEIGHT // 2
 
 
     def draw(self):
@@ -20,3 +21,7 @@ class Ship(Flying_Object):
     # def move_right(self):
 
     # def move_left(self):
+
+    def hit(self):
+
+        self.alive = False

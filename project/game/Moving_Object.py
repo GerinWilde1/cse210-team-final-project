dcc8578add6a31578035d226375239dc65604a0e
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-18 10:23:31 -0600

    updates

---
+++ project/game/Moving_Object.py
@@ -0,0 +1,35 @@
+import constants
+import Point
+
+class Flying_Object:
+    
+    def __init__(self):
+        """
+            All of the info needed to cary into all the other everything else
+        """
+        self.center = Point.Point(0.0,0.0)
+        self.velocity = Point.Velocity(0.0,0.0)
+        self.angle = 0
+        self.rotate = 0
+        self.rotation = 0
+        self.alive = True
+        self.rotate += self.rotation
+        if self.rotate == 359:
+            self.rotate = 0.0
+        
+    def advance(self):
+        """
+            For movement
+        """
+        self.center.x += self.velocity.dx
+        self.center.y += self.velocity.dy
+        
+    def is_offscreen(self):
+        if self.center.x > constants.SCREEN_WIDTH:
+            self.center.x = 0.0
+        if self.center.y > constants.SCREEN_HEIGHT:
+            self.center.y = 0.0
+        if self.center.x < 0.0:
+            self.center.x = constants.SCREEN_WIDTH
+        if self.center.y < 0.0:
+            self.center.y = constants.SCREEN_HEIGHT
---
+++ project/game/Velocity.py
@@ -0,0 +1,7 @@
+class Velocity:
+    """
+        Keeps track of the motion of the item
+    """
+    def __init__(self, init_dx, init_dy):
+        self.dx = init_dx
+        self.dy = init_dy

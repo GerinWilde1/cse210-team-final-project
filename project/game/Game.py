d36309914b15e47c50ed256aa81339faa47f1bde
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-19 10:30:56 -0600

    Update Game.py

--- project/game/Game.py
+++ project/game/Game.py
@@ -14,7 +14,51 @@ class Game(arcade.Window):
 
     def __init__(self, width, height):
 
+
+        super().__init__(width, height)
+        arcade.set_background_color(arcade.color.SMOKY_BLACK)
+
+        self.held_keys = set()
+
+
+        self.ship = Your_Ship.Ship()
+        self.bullets = []
+        self.enemy_ships = []
+
+
+
+
+
     def on_draw(self):
+
+        """
+        Called automatically by the arcade framework.
+        Handles the responsibility of drawing all elements.
+        """
+        
+        # clear the screen to begin drawing
+        arcade.start_render()
+        
+        
+        
+
+        
+        for bullet in self.bullets:
+            bullet.draw()
+        
+        if self.ship.alive:
+            self.ship.draw()
+        else:
+            self.draw_gameover()
+
+        
+        
+        for asteroid in self.asteroids:
+            asteroid.draw()
+            
+             
+
+
 
     def update(self, delta_time):
 
@@ -24,7 +68,7 @@ class Game(arcade.Window):
 
     def cleanup_zombies(self):
 
-    def break_apart(self):
+    # def break_apart(self):
 
     def draw_gameover(self):
 

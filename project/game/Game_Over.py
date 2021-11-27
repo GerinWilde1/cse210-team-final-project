e61a78d0abd83a5a82af1f8e8f627f045d4adee8
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-27 11:48:42 -0600

    update

--- project/game/Game_Over.py
+++ project/game/Game_Over.py
@@ -8,7 +8,7 @@ class Game_Over(arcade.View):
     def __init__(self):
         """This is run once the player dies/we switch to this view"""
         super().__init__()
-        # self.texture = arcade.load_animated_gif("game.Roll.gif")
+        
         self.background = arcade.load_texture("project/game/Roll.gif")
 
         # Reset the viewport, necessaey if we have a scrolling game and we need
@@ -22,15 +22,16 @@ class Game_Over(arcade.View):
     def on_draw(self):
         """Draw this View"""
         arcade.start_render()
+        #sets backbround image
         arcade.draw_texture_rectangle(self.window.width/2, self.window.height/2, self.window.width, self.window.height, self.background)
-        # arcade.texture.draw_sized(self.screen_width, self.screen_height, self.screen_width / 2, self.screen_height / 2)
+        #draws text on screen
         arcade.draw_text("GAME OVER", self.window.width / 2, self.window.height / 2, arcade.color.WHITE, font_size=50, anchor_x="center")
         arcade.draw_text("Click to restart", self.window.width / 2, self.window.height / 2-75, arcade.color.WHITE, font_size=20, anchor_x="center")
 
 
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
-        
+        """on mouse press start the game over"""
         game_view = game.Game()
         self.window.show_view(game_view)
         game_view.setup()

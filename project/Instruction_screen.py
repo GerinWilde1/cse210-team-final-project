38240202def94099913c46d380e7c14a5a5ea316
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-11-27 11:52:28 -0600

    update

--- project/Instruction_screen.py
+++ project/Instruction_screen.py
@@ -9,8 +9,8 @@ class InstrustionView(arcade.View):
 
     def on_show(self):
         """This id run once we switch to this view"""
-        # self.background = arcade.load_texture("project/game/Roll.gif")
-        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+        self.background = arcade.load_texture("project/game/black_hole.jpg")
+        # arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
         
         # Reset the viewport, necessaey if we have a scrolling game and we need
         # to reset the viewport back to the start do we can see what we draw
@@ -20,7 +20,7 @@ class InstrustionView(arcade.View):
     def on_draw(self):
         """Draw this View"""
         arcade.start_render()
-        
+        arcade.draw_texture_rectangle(self.window.width/2, self.window.height/2, self.window.width, self.window.height, self.background)
         # Draws opening words to screen
         arcade.draw_text("Opening Screen", self.window.width / 2, self.window.height / 2, arcade.color.WHITE, font_size=50, anchor_x="center")
         arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2-75, arcade.color.WHITE, font_size=20, anchor_x="center")
